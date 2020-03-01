f14aa6a949ed6564fd7674604dd6eb55da1ea2b0
ZVLU <zvluwork@gmail.com>
2020-02-29 20:35:40 -0500

    controllers finished

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,57 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger");
+
+
+router.get("/", function(req, res){
+    burger.selectAll(function(data){
+        var hbsObject = {
+            burgers: data
+        };
+    });
+});
+
+router.post("/api/burgers", function(req, res){
+    burger.insertOne([
+        "burger_name", "devoured"
+    ],[
+        req.body.name, req.body.devoured
+    ],function(result){
+        res.json({id:result.insertId});
+    });
+});
+
+router.put("/api/burgers/:id",function(req,res){
+    var condition = "id = " + req.params.id;
+
+    burger.updateOne(
+        "devoured", true,
+        "id", req.params.id,
+        function(result) {
+
+           if(result.changedRows == 0) {
+                return res.status(404).end();   
+           } else {
+               res.status(200).end();
+           }
+     }); 
+        
+    
+});
+
+router.delete("/api/burgers/:id", function(req,res){
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function(results) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+
+module.exports = router;
